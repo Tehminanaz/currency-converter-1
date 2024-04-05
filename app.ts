f63d1cb10b9d43da33f4e061bef597a68edbb227
Tehminanaz <tn0457584@gmail.com>
2024-04-05 14:45:02 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,41 @@
+import inquirer from "inquirer"
+
+const currency : any = {
+    USD : 1,
+    EUR : 0.91,
+    GBU : 0.76,
+    INR : 74.57,
+    PKR : 280
+
+}
+
+let user_answer = await inquirer.prompt(
+    [
+        {
+            name: "from",
+            type: "list",
+            message : "Enter from currency",
+            choices : ["USD", "EUR", "GBU", "INR", "PKR"] 
+            
+        },
+        {
+            name: "to",
+            type: "list",
+            message : "Enter from currency",
+            choices : ["USD", "EUR", "GBU", "INR", "PKR"] 
+            
+        },
+        {
+            name: "amount",
+            type: "input",
+            message : "Enter your ammount",
+            
+        }
+    ]
+)
+let fromAmount = currency [user_answer.from];
+let toamount = currency [user_answer.to];
+let amount = user_answer.amount
+let baseAmount = amount / fromAmount 
+let convertedAmount = baseAmount *toamount
+ console.log(convertedAmount);
